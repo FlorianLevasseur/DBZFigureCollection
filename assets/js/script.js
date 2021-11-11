f807b5044dd470f22fcdf10d659901b9af74fd48
Florian <florian.levasseur@hotmail.fr>
2021-11-11 18:52:10 +0100

    1er test mode sombre JS

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     document.getElementById("myLink").href = "assets/css/bootstrap.css"
+//     document.getElementById("myAccount").classList.replace("account","accountDark")
+//     document.getElementById("myNav").classList.replace("bg-light","bg-dark")
+//     document.getElementById("myNav").classList.replace("navbar-light","navbar-dark")
+//     document.getElementById("contentId").classList.replace("bg-white","bg-dark")
+//     document.getElementById("foot1").classList.replace("footLink","footLinkDark")
+//     document.getElementById("foot2").classList.replace("footLink","footLinkDark")
+//     document.getElementById("foot3").classList.replace("footLink","footLinkDark")
+// }
